c0ed433b5539c3873c79ebb8ce7f0a20e842d846
Computer Mann <112242931+Computermann8086@users.noreply.github.com>
2025-11-06 11:25:49 +0100

    Refactor variable name in fast_sqrt function
    Replace variable 'i' with 'nf' for clarity in fast_sqrt function.

--- sqrt.c
+++ sqrt.c
@@ -1,9 +1,9 @@
 float fast_sqrt(float n){
     float est = n;
-    long i;
+    long nf;
 
-    i = *(long*)&est;         // Make it a long, so we can actually do shit with the bits
-    i = (((i >> 1)<<23)-1064992506)^2147483648U;   // Why does this even work?
+    nf = *(long*)&est;         // Make it a long, so we can actually do shit with the bits
+    nf = (((i >> 1)<<23)-1064992506)^2147483648U;   // Why does this even work?
     est = *(float*)&i;
     est = 0.5F*(est+(n/est)); // Two iterations of Newtons Method
     est = 0.5F*(est+(n/est)); // Here's the seccond one
