9e012154f697fcffac89dbcca7ba1953d9938f60
Computer Mann <112242931+Computermann8086@users.noreply.github.com>
2025-11-06 13:52:20 +0100

    Update sqrt.c

--- sqrt.c
+++ sqrt.c
@@ -4,7 +4,7 @@ float fast_sqrt(float n){
 
     nf = *(long*)&est;         // Make it a long, so we can actually do shit with the bits
     nf = (((i >> 1)<<23)-1064992506)^2147483648U;   // Why does this even work?
-    est = *(float*)&i;
+    est = *(float*)&nf;
     est = 0.5F*(est+(n/est)); // Two iterations of Newtons Method
     est = 0.5F*(est+(n/est)); // Here's the seccond one
     return est;
